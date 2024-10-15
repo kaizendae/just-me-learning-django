040fb9c9f8ea7e8d13e63e9a5667a6a88c9324ba
kaizendae <abdelattie@gmail.com>
2024-10-15 13:53:29 +0100

    Enhance the model in the choice and question with tabular inline

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
-admin.site.register(Question)
 admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
